b542de44d1c5fb97d29ef1ed91f12cd91fb54f21
belobelov <139685037+belobelov@users.noreply.github.com>
2023-12-22 12:14:53 +0200

    Update header.test.js

--- tests/ui/header.test.js
+++ tests/ui/header.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check header', async ({ page }) => {
-    await page.goto('http://localhost:8090'); 
+    await page.goto('https://beloslavbelov-exam-3pxd.onrender.com'); 
     const homeLink = await page.$('h1');
     const text = await homeLink.textContent();
     expect(text).toBe('My Grades');
