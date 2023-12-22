2cca4fa61e1745caa59f42cd94ec3e79f7690369
belobelov <139685037+belobelov@users.noreply.github.com>
2023-12-22 12:13:48 +0200

    Update add-grade.test.js

--- tests/ui/add-grade.test.js
+++ tests/ui/add-grade.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check add grades page', async ({ page }) => {
-    await page.goto('http://localhost:8090/My-Grades');
+    await page.goto('https://beloslavbelov-exam-3pxd.onrender.com/My-Grades');
     const form = await page.$('ul');
     expect(form).toBeTruthy();
   });
